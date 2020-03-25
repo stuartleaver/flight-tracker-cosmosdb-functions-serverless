61bb3cb9f8ccaaa181bbcb054b1892fa411583eb
stuartleaver <stuartleaveruk@gmail.com>
2020-03-25 19:35:17 +0000

    Add map to the web app
    Updated the web app so that it displays the map. Currently it is set to the centre of the UK roughly.
    
    While the primary key of the Map resource is visible, it is retrieved from the KeyVault as an example on how an App Service can get config from a KeyVault via Managed Identities.

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 
 namespace flighttracker
@@ -12,6 +16,21 @@ namespace flighttracker
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var builtConfig = config.Build();
+
+                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                    var KeyVaultClient = new KeyVaultClient(
+                        new KeyVaultClient.AuthenticationCallback(
+                            azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                    config.AddAzureKeyVault(
+                        $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/",
+                        KeyVaultClient,
+                        new DefaultKeyVaultSecretManager()
+                    );
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
