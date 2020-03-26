c3e64a003cb41f73caba3018883b74cb9fe9e9f7
stuartleaver <stuartleaveruk@gmail.com>
2020-03-26 20:27:28 +0000

    Add function to get initial flight data
    Created the function that will return the data held in the Cosmos DB container.

---
+++ src/functions/GetFlightData.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace flighttracker.functions
+{
+    public static class GetFlightData
+    {
+        [FunctionName("GetFlightData")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "getflightdata")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "flighttracker",
+                collectionName: "flights",
+                SqlQuery = "SELECT * FROM c",
+                ConnectionStringSetting = "CosmosDbConnectionString")] IEnumerable<Flight> documents,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new OkObjectResult(documents);
+        }
+    }
+}
